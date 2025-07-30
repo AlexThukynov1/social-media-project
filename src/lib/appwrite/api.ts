025098db6fecaa8790d93c05625edca4156902a5
AlexThukynov1 <alexthukynov@gmail.com>
2025-07-30 12:06:42 +0300

    add createUserAccount

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,3 +1,18 @@
-export async function createUserAccount(user) {
+import {ID} from 'appwrite'
+import type {INewUser} from "@/types";
+import {account} from "@/lib/appwrite/appwrite-config.ts";
 
+export async function createUserAccount(user: INewUser) {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name
+        );
+
+        return newAccount;
+    } catch (error) {
+        return error;
+    }
 }
