fc9e01694c4acd5c2801c3aa47fe2792af0d5c16
AlexThukynov1 <alexthukynov@gmail.com>
2025-07-31 18:15:50 +0300

    user to db

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import {ID} from 'appwrite'
 import type {INewUser} from "@/types";
-import {account} from "@/lib/appwrite/appwrite-config.ts";
+import {account, appwriteConfig, avatars, databases} from "@/lib/appwrite/appwrite-config.ts";
 
 export async function createUserAccount(user: INewUser) {
     try {
@@ -11,8 +11,41 @@ export async function createUserAccount(user: INewUser) {
             user.name
         );
 
-        return newAccount;
+        if(!newAccount) throw Error;
+
+        const avatarUrl = avatars.getInitials(user.name)
+
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarUrl,
+        })
+        return newUser;
     } catch (error) {
         return error;
+    }
+}
+
+export async function saveUserToDB(user: {
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+}) {
+    try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.usersCollectionId,
+            ID.unique(),
+            user,
+
+        )
+
+        return newUser;
+    } catch (error) {
+        console.log(error);
     }
 }
