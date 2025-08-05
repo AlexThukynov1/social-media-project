a3ea9adefb9d65947c7acd549251baa647748da0
AlexThukynov1 <alexthukynov@gmail.com>
2025-08-05 16:52:07 +0300

    hotfix

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import App from "./App.tsx";
 import {BrowserRouter} from "react-router-dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthProvider from "@/context/AuthContext.tsx";
+import {QueryProvider} from "@/lib/react-query/QueryProvider.tsx";
 
-const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
-    <QueryClientProvider client={queryClient}>
+    <QueryProvider>
         <BrowserRouter>
-            <App/>
+            <AuthProvider>
+                <App/>
+            </AuthProvider>
         </BrowserRouter>
-    </QueryClientProvider>
-
+    </QueryProvider>
 )
