f6b609e00d83d434a56287f9e4be90f97c0e34fb
AlexThukynov1 <alexthukynov@gmail.com>
2025-09-30 15:13:55 +0300

    fix  hooks error

--- src/_root/pages/ExplorePage.tsx
+++ src/_root/pages/ExplorePage.tsx
@@ -1,42 +1,59 @@
-import GridPostList from "@/components/common/GridPostList";
-import Loader from "@/components/common/Loader";
-import SearchResult from "@/components/common/SearchResult";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
 import { Input } from "@/components/ui/input";
 import useDebounce from "@/hooks/useDebounce";
+import Loader  from "@/components/common/Loader";
+import GridPostList from "@/components/common/GridPostList";
 import { useGetPostsQuery, useSearchPostsQuery } from "@/lib/react-query/queriesAndMutations";
-import { useEffect, useState } from "react";
-import {useInView} from 'react-intersection-observer'
 
+export type SearchResultProps = {
+  isSearchFetching: boolean;
+  searchedPosts: any;
+};
 
-export default function ExplorePage() {
-  const [searchValue, setSearchValue] = useState('');
-  const {ref, inView} = useInView()
+const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
+  if (isSearchFetching) {
+    return <Loader />;
+  } else if (searchedPosts && searchedPosts.documents.length > 0) {
+    return <GridPostList posts={searchedPosts.documents} />;
+  } else {
+    return (
+      <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+    );
+  }
+};
 
-  const {data: posts, fetchNextPage, hasNextPage} = useGetPostsQuery()
-  const debauncedValue = useDebounce(searchValue, 500)
-  const {data: searchedPost, isFetching: isSearchFetch} = useSearchPostsQuery(debauncedValue)
+const ExplorePage = () => {
+  const { ref, inView } = useInView();
+  const { data: posts, fetchNextPage, hasNextPage } = useGetPostsQuery();
 
-  const showShowSearchResults = searchValue !== '';
-  const shouldShowPosts = !showShowSearchResults 
-        && posts?.pages.every((item) => item?.documents.length === 0)
-  
-  if(!posts) {
+  const [searchValue, setSearchValue] = useState("");
+  const debouncedSearch = useDebounce(searchValue, 500);
+  const { data: searchedPosts, isFetching: isSearchFetching } = useSearchPostsQuery(debouncedSearch);
+
+  useEffect(() => {
+    if (inView && !searchValue) {
+      fetchNextPage();
+    }
+  }, [inView, searchValue]);
+
+  if (!posts)
     return (
       <div className="flex-center w-full h-full">
-        <Loader/>
+        <Loader />
       </div>
-    )
-  }
+    );
 
-  useEffect(()=>{
-    if(inView && !searchValue) fetchNextPage()
-  }, [inView, searchValue])
+  const shouldShowSearchResults = searchValue !== "";
+  const shouldShowPosts = !shouldShowSearchResults && 
+    posts.pages.every((item) => item && item.documents.length === 0);
 
   return (
-    <div className="explore-container">
-      <div className="explore-inner_container">
-        <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2> 
-        <div className="flex gap-1 px-4 w-full bg-dark-4">
+    <div className="ExplorePage-container">
+      <div className="ExplorePage-inner_container">
+        <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
           <img
             src="/assets/icons/search.svg"
             width={24}
@@ -46,16 +63,20 @@ export default function ExplorePage() {
           <Input
             type="text"
             placeholder="Search"
-            className="explore-search"
+            className="ExplorePage-search"
             value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
+            onChange={(e) => {
+              const { value } = e.target;
+              setSearchValue(value);
+            }}
           />
         </div>
       </div>
 
       <div className="flex-between w-full max-w-5xl mt-16 mb-7">
-         <h2 className="body-bold md:h3-bold">Popular today</h2>
-         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+        <h3 className="body-bold md:h3-bold">Popular Today</h3>
+
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
           <p className="small-medium md:base-medium text-light-2">All</p>
           <img
             src="/assets/icons/filter.svg"
@@ -63,32 +84,33 @@ export default function ExplorePage() {
             height={20}
             alt="filter"
           />
-         </div>
+        </div>
       </div>
 
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
-        {showShowSearchResults ? (
-          <SearchResult
-            isSearchFetching={isSearchFetch}
-            searchedPost={searchedPost?.documents ?? []}
+        {shouldShowSearchResults ? (
+          <SearchResults
+            isSearchFetching={isSearchFetching}
+            searchedPosts={searchedPosts}
           />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
-        ): posts.pages.map((item, index) =>
-          item ? (
-            <GridPostList
-              key={`page-${index}`}
-              posts={item.documents}
-            />
-          ) : null
+        ) : (
+          posts.pages.map((item, index) =>
+            item ? (
+              <GridPostList key={`page-${index}`} posts={item.documents} />
+            ) : null
+          )
         )}
       </div>
 
-      {hasNextPage && searchValue && (
+      {hasNextPage && !searchValue && (
         <div ref={ref} className="mt-10">
-          <Loader/>
+          <Loader />
         </div>
       )}
     </div>
-  )
-}
+  );
+};
+
+export default ExplorePage;
